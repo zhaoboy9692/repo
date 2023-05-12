540f226aae38c210cc8199d3f855b6e8873c9aee
zhaoxincheng <zhaoboy9692@163.com>
2023-05-12 14:53:42 +0800

    1

--- update.sh
+++ update.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-dpkg-scanpackages -m ./debs > ./Packages
-bzip2 -fks ./Packages
-
+dpkg-scanpackages -m ./debs > ./Packages && bzip2 -c9 Packages > Packages.bz2
+xz -c9 Packages > Packages.xz
+xz -5fkev --format=lzma Packages > Packages.lzma
+lz4 -c9 Packages > Packages.lz4
+gzip -c9 Packages > Packages.gz
+zstd -c19 Packages > Packages.zst && git add . && git commit -m "1" && git push
 echo "生成成功！"
