cb96d16a1fbb7ea8c685efedae405ca19ba85a45
zhaoxincheng <zhaoboy9692@163.com>
2023-05-12 14:54:51 +0800

    提交更新

--- update.sh
+++ update.sh
@@ -4,5 +4,5 @@ xz -c9 Packages > Packages.xz
 xz -5fkev --format=lzma Packages > Packages.lzma
 lz4 -c9 Packages > Packages.lz4
 gzip -c9 Packages > Packages.gz
-zstd -c19 Packages > Packages.zst && git add . && git commit -m "1" && git push
+zstd -c19 Packages > Packages.zst && git add . && git commit -m "提交更新" && git push
 echo "生成成功！"
